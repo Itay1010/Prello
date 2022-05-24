af303a8f440736168b106ba592ba358440275d46
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-24 19:16:47 +0300

    added components

---
+++ src/cmps/board/task-list.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+
+export const TaskList = () => {
+    return <div>I am the task list</div>
+}
---
+++ src/store/root.reducer.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux"
+import thunk from "redux-thunk"
+import { userReducer } from "./user/user.reducer"
+import { boardReducer } from './board/board.reducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+
+const rootReducer = combineReducers({
+    userModule: userReducer,
+    boardModule: boardReducer
+})
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+window.myStore = store
