599f785677dbc15fd943516bc072f094c3f605c1
Itay Zonshine <ezonsh@email.com>
2022-05-25 00:09:17 +0300

    sync

--- src/pages/workspace.jsx
+++ src/pages/workspace.jsx
@@ -3,5 +3,30 @@ import { BoardList } from '../cmps/workspace/board-list'
 
 
 export const Workspace = () => {
-    return <section>I am the workspace page</section>
+    return <section className="workspace-wrapper">
+        <section className="board-container">
+            <section className="board-list-header">container header</section>
+            <section className="board-list">
+                <div>board</div>
+                <div>board</div>
+                <div>board</div>
+            </section>
+        </section>
+        <section className="board-container">
+            <section className="board-list-header">container header</section>
+            <section className="board-list">
+                <div>board</div>
+                <div>board</div>
+                <div>board</div>
+            </section>
+        </section>
+        <section className="board-container">
+            <section className="board-list-header">container header</section>
+            <section className="board-list">
+                <div>board</div>
+                <div>board</div>
+                <div>board</div>
+            </section>
+        </section>
+    </section>
 }
