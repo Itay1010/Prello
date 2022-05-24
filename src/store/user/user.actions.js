a485a9aa95189a83bdcfdebf67c122c0611435eb
Itay Zonshine <ezonsh@email.com>
2022-05-24 16:58:42 +0300

    folder routing

---
+++ src/store/user/user.actions.js
@@ -0,0 +1,94 @@
+import { userService } from "../services/user.service.js";
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+import { socketService, SOCKET_EMIT_USER_WATCH, SOCKET_EVENT_USER_UPDATED } from "../services/socket.service.js";
+
+export function loadUsers() {
+    return async dispatch => {
+        try {
+            dispatch({ type: 'LOADING_START' })
+            const users = await userService.getUsers()
+            dispatch({ type: 'SET_USERS', users })
+        } catch (err) {
+            console.log('UserActions: err in loadUsers', err)
+        } finally {
+            dispatch({ type: 'LOADING_DONE' })
+        }
+    }
+}
+
+export function removeUser(userId) {
+    return async dispatch => {
+        try {
+            await userService.remove(userId)
+            dispatch({ type: 'REMOVE_USER', userId })
+        } catch (err) {
+            console.log('UserActions: err in removeUser', err)
+        }
+    }
+}
+
+export function onLogin(credentials) {
+    return async (dispatch) => {
+        try {
+            const user = await userService.login(credentials)
+            dispatch({
+                type: 'SET_USER',
+                user
+            })
+        } catch (err) {
+            showErrorMsg('Cannot login')
+            console.log('Cannot login', err)
+        }
+    }
+}
+
+
+export function onSignup(credentials) {
+    return async (dispatch) => {
+        try {
+            const user = await userService.signup(credentials)
+            dispatch({
+                type: 'SET_USER',
+                user
+            })
+        } catch (err) {
+            showErrorMsg('Cannot signup')
+            console.log('Cannot signup', err)
+        }
+
+    }
+}
+
+export function onLogout() {
+    return async (dispatch) => {
+        try {
+            await userService.logout()
+            dispatch({
+                type: 'SET_USER',
+                user: null
+            })
+        } catch (err) {
+            showErrorMsg('Cannot logout')
+            console.log('Cannot logout', err)
+        }
+    }
+}
+
+export function loadAndWatchUser(userId) {
+    return async (dispatch) => {
+        try {
+            const user = await userService.getById(userId);
+            dispatch({ type: 'SET_WATCHED_USER', user })
+            socketService.emit(SOCKET_EMIT_USER_WATCH, userId)
+            socketService.off(SOCKET_EVENT_USER_UPDATED)
+            socketService.on(SOCKET_EVENT_USER_UPDATED, user => {
+                showSuccessMsg(`This user ${user.fullname} just got updated from socket, new score: ${user.score}`)
+                dispatch({ type: 'SET_WATCHED_USER', user })
+            })
+        } catch (err) {
+            showErrorMsg('Cannot load user')
+            console.log('Cannot load user', err)
+        }
+    }
+}
+
