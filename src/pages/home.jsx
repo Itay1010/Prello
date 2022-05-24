e5f9d8ca5050d971823d610b77bfa25a95451d4c
Itay Zonshine <ezonsh@email.com>
2022-05-24 22:47:51 +0300

    started work on task preview cmp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,5 @@ import { TaskPreview } from "../cmps/board/task-preview"
 export const Home = () => {
     return <section>
         <h1>I am the home page</h1>
-        <TaskPreview />
-
     </section>
 }
