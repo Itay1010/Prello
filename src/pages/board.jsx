a646c7817c0eaa87ae20fec66be849e8ee483eac
Offir Carmi <offircarmi@gmail.com>
2022-05-24 22:46:36 +0300

    basic scss

--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -1,7 +1,28 @@
 import React from "react"
+import { MainHeader } from "../cmps/shared cmps/main-header"
 
 import { TaskList } from '../cmps/workspace/board-list'
 
 export const Board = () => {
-    return <section>I am the board page</section>
+    return <React.Fragment>
+        <MainHeader />
+        <section className="board">
+            <header>
+                Board's header
+            </header>
+            <section className="group-container flex">
+                <section className="group">
+                    group
+                </section>
+                <section className="group">
+                    group
+                </section>
+                <section className="group">
+                    group
+                </section>
+            </section>
+        </section>
+
+    </React.Fragment>
+
 }
