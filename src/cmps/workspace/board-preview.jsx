a2a8bc62fe5194bbe75419b2492d9ddf3ff93979
Itay Zonshine <ezonsh@email.com>
2022-05-25 01:12:19 +0300

    started workspace layout

--- src/cmps/workspace/board-preview.jsx
+++ src/cmps/workspace/board-preview.jsx
@@ -1,5 +1,15 @@
 import React from "react"
 
 export const BoardPreview = () => {
-    return <div>I am the board preview</div>
+    return <section className="board-container">
+        <section className="board-preview-header">
+            <h4>icon</h4>
+            <h1>Tag name</h1>
+        </section>
+        <section className="board-preview-body">
+            <div>board</div>
+            <div>board</div>
+            <div>board</div>
+        </section>
+    </section>
 }
--- src/pages/workspace.jsx
+++ src/pages/workspace.jsx
@@ -1,32 +1,11 @@
 import React from "react"
+import { MainHeader } from "../cmps/shared cmps/main-header"
 import { BoardList } from '../cmps/workspace/board-list'
 
 
 export const Workspace = () => {
     return <section className="workspace-wrapper">
-        <section className="board-container">
-            <section className="board-list-header">container header</section>
-            <section className="board-list">
-                <div>board</div>
-                <div>board</div>
-                <div>board</div>
-            </section>
-        </section>
-        <section className="board-container">
-            <section className="board-list-header">container header</section>
-            <section className="board-list">
-                <div>board</div>
-                <div>board</div>
-                <div>board</div>
-            </section>
-        </section>
-        <section className="board-container">
-            <section className="board-list-header">container header</section>
-            <section className="board-list">
-                <div>board</div>
-                <div>board</div>
-                <div>board</div>
-            </section>
-        </section>
+        <MainHeader />
+        <BoardList />
     </section>
 }
