83189251b80eebd16bb09acd7c14b46976835402
Itay Zonshine <ezonsh@email.com>
2022-05-24 20:07:39 +0300

    finished routing

--- src/root.cmp.js
+++ src/root.cmp.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { routes } from './routes'
 
 export default function App() {
-
   return <div className='app'>
+    <Switch>
+      {routes.map(route => <Route key={route.path} component={route.component} path={route.path} />)}
+    </Switch>
   </div>
 }
