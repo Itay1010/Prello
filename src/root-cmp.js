99bcc7dfd3ef0d45b2785a9ce681bd87369fa71c
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-24 16:19:00 +0300

    first commit for project

--- src/root-cmp.js
+++ src/root-cmp.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { WhoWatch } from './pages/who-watch'
 
 export default function App() {
   return <div className='app'>
-    <h1>WHO WATCH???</h1>
-    <WhoWatch />
+
   </div>
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux"
 import thunk from "redux-thunk"
-import { watcherReducer } from "../store/watcher.reducer"
+// import { watcherReducer } from "../store/watcher.reducer"
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 
 const rootReducer = combineReducers({
-    watcherModule: watcherReducer,
+    // watcherModule: watcherReducer,
 })
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
