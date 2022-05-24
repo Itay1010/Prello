8f93ca6b07ee5d5aa75c2fb123d971148ac8e922
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-25 00:11:26 +0300

    added user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
-import { storageService } from './async-storage.service'
+import { storageService } from './basic/async-storage.service'
 import { httpService } from './http.service'
-import { socketService, SOCKET_EVENT_USER_UPDATED } from './socket.service'
+// import { socketService, SOCKET_EVENT_USER_UPDATED } from './socket.service'
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
 var gWatchedUser = null;
 
@@ -14,49 +14,48 @@ export const userService = {
     getById,
     remove,
     update,
-    changeScore
 }
 
 window.userService = userService
 
 
 function getUsers() {
-    // return storageService.query('user')
-    return httpService.get(`user`)
+    return storageService.query('user')
+    // return httpService.get(`user`)
 }
 
 async function getById(userId) {
-    // const user = await storageService.get('user', userId)
-    const user = await httpService.get(`user/${userId}`)
+    const user = await storageService.get('user', userId)
+    // const user = await httpService.get(`user/${userId}`)
     // gWatchedUser = user;
     return user;
 }
 function remove(userId) {
-    // return storageService.remove('user', userId)
-    return httpService.delete(`user/${userId}`)
+    return storageService.remove('user', userId)
+    // return httpService.delete(`user/${userId}`)
 }
 
 async function update(user) {
-    // await storageService.put('user', user)
-    user = await httpService.put(`user/${user._id}`, user)
+    await storageService.put('user', user)
+    // user = await httpService.put(`user/${user._id}`, user)
     // Handle case in which admin updates other user's details
-    if (getLoggedinUser()._id === user._id) saveLocalUser(user)
+    // if (getLoggedinUser()._id === user._id) saveLocalUser(user)
     return user;
 }
 
 async function login(userCred) {
-    // const users = await storageService.query('user')
-    // const user = users.find(user => user.username === userCred.username)
-    // return _saveLocalUser(user)
+    const users = await storageService.query('user')
+    const user = users.find(user => user.username === userCred.username)
+    return saveLocalUser(user)
 
-    const user = await httpService.post('auth/login', userCred)
+    // const user = await httpService.post('auth/login', userCred)
     // socketService.emit('set-user-socket', user._id);
     if (user) return saveLocalUser(user)
 }
 async function signup(userCred) {
     userCred.score = 10000;
-    // const user = await storageService.post('user', userCred)
-    const user = await httpService.post('auth/signup', userCred)
+    const user = await storageService.post('user', userCred)
+    // const user = await httpService.post('auth/signup', userCred)
     // socketService.emit('set-user-socket', user._id);
     return saveLocalUser(user)
 }
@@ -86,15 +85,28 @@ function getLoggedinUser() {
 
 
 // ;(async ()=>{
-//     await userService.signup({fullname: 'Puki Norma', username: 'user1', password:'123',score: 10000, isAdmin: false})
-//     await userService.signup({fullname: 'Master Adminov', username: 'admin', password:'123', score: 10000, isAdmin: true})
-//     await userService.signup({fullname: 'Muki G', username: 'muki', password:'123', score: 10000})
-// })()
-
-
-// This is relevant when backend is connected
-// ;(async () => {
-//     var user = getLoggedinUser()
-//     if (user) socketService.emit('set-user-socket', user._id)
-// })();
-
+    //     await userService.signup({_id: u101,fullname: 'Itay Zonshine', username: 'itay', password:'itay', isAdmin: false})
+    //     await userService.signup({_id: u102,fullname: 'Offir Carmi', username: 'offir', password:'offir', isAdmin: true})
+    //     await userService.signup({_id: u103,fullname: 'Eytan Silberberg', username: 'eytan', password:'eytan',isAdmin: false})
+    // })()
+    
+    
+    // This is relevant when backend is connected
+    // ;(async () => {
+        //     var user = getLoggedinUser()
+        //     if (user) socketService.emit('set-user-socket', user._id)
+        // })();
+        
+        
+        // const user = {
+        //     "_id": "u101",
+        //     "fullname": "Abi Abambi",
+        //     "username": "abi@ababmi.com",
+        //     "password": "aBambi123",
+        //     "imgUrl": "http://some-img.jpg",
+        //     "mentions": [{
+        //         "id": "m101",
+        //         "boardId": "m101",
+        //         "taskId": "t101"
+        //     }]
+        // }
