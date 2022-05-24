5b90d38d754df140859e3d51829840b9b776109f
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-25 01:11:29 +0300

    added content in home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,12 @@ import React from "react"
 import { TaskPreview } from "../cmps/board/task-preview"
 
 export const Home = () => {
-    return <section>
-        <h1>I am the home page</h1>
+    return <section className="home">
+        <nav className="flex space-between"><div className="logo">Logo</div><div className="auth-links">Auth links</div></nav>
+        <div className="hero">
+            <h1>While you grow, save yourself the trouble with Prello</h1>
+            <p>Preparation is the key name, as a freelancer to enterprises Prello is the tool to keep yourself in ease.</p>
+            <button>Come and explore</button>
+        </div>
     </section>
 }
