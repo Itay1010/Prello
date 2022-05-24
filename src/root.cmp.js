a0a51efe267860ececa9bfd4e0197349b4d28d2f
Itay Zonshine <ezonsh@email.com>
2022-05-24 20:09:08 +0300

    finished routing

--- src/root.cmp.js
+++ src/root.cmp.js
@@ -6,7 +6,7 @@ import { routes } from './routes'
 export default function App() {
   return <div className='app'>
     <Switch>
-      {routes.map(route => <Route key={route.path} component={route.component} path={route.path} />)}
+      {routes.map(route => <Route key={route.path} exact={true} component={route.component} path={route.path} />)}
     </Switch>
   </div>
 }
