5cfc85c53e65b2ed27f43e32644f5a3e103aa0ed
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-24 19:12:54 +0300

    added coponents

--- src/cmps/browsing/browsing.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const Browsing = () => {
-    return <section>I am the browsing section</section>
-}
--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -1,1 +1,7 @@
 import React from "react"
+
+import { TaskList } from '../cmps/workspace/board-list'
+
+export const Board = () => {
+    return <section>I am the board page</section>
+}
