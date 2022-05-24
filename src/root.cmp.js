fd0f2ac55adfdabf0dceaf43964490617521ed67
Itay Zonshine <ezonsh@email.com>
2022-05-24 20:18:38 +0300

    routing fixs

--- src/root.cmp.js
+++ src/root.cmp.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import { routes } from './routes'
 
 export default function App() {
   return <div className='app'>
     <Switch>
-      {routes.map(route => <Route key={route.path} exact={true} component={route.component} path={route.path} />)}
+      {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
     </Switch>
   </div>
 }
