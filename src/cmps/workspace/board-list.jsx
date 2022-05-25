2925f47f377a1a65f3ae85e9c774d28808d41ad5
Itay Zonshine <ezonsh@email.com>
2022-05-25 09:48:38 +0300

    more work on workspace

--- src/cmps/workspace/board-list.jsx
+++ src/cmps/workspace/board-list.jsx
@@ -4,6 +4,26 @@ import { BoardPreview } from './board-preview'
 
 export const BoardList = () => {
     return <div className="workspace-container">
-        {[<BoardPreview />, <BoardPreview />, <BoardPreview />]}
+        <section className="board-container">
+            <section className="board-preview-header">
+                <h4>Star icon</h4>
+                <h1>Starred boards</h1>
+            </section>
+            <BoardPreview count={2}/>
+        </section>
+        <section className="board-container">
+            <section className="board-preview-header">
+                <h4>icon</h4>
+                <h1>My boards</h1>
+            </section>
+            <BoardPreview count={1}/>
+        </section>
+        <section className="board-container">
+            <section className="board-preview-header">
+                <h4>icon</h4>
+                <h1>Templates</h1>
+            </section>
+            <BoardPreview count={5}/>
+        </section>
     </div>
 }
--- src/cmps/workspace/board-preview.jsx
+++ src/cmps/workspace/board-preview.jsx
@@ -1,15 +1,7 @@
 import React from "react"
 
-export const BoardPreview = () => {
-    return <section className="board-container">
-        <section className="board-preview-header">
-            <h4>icon</h4>
-            <h1>Tag name</h1>
-        </section>
-        <section className="board-preview-body">
-            <div>board</div>
-            <div>board</div>
-            <div>board</div>
-        </section>
+export const BoardPreview = ({ count }) => {
+    return <section className="board-preview-body">
+        {new Array(count).fill(<div></div>, 0)}
     </section>
 }
