f423df66e70fcfabba8af46bb02a5ba755971c7c
Itay Zonshine <ezonsh@email.com>
2022-05-25 11:18:58 +0300

    added json file for bords

--- src/cmps/workspace/board-list.jsx
+++ src/cmps/workspace/board-list.jsx
@@ -3,27 +3,30 @@ import React from "react"
 import { BoardPreview } from './board-preview'
 
 export const BoardList = () => {
+    const starred = 1
+    const myBoards = 1
+    const templates = 5
     return <div className="workspace-container">
-        <section className="board-container">
+        {starred > 0 && <section className="board-container">
             <section className="board-preview-header">
                 <h4>Star icon</h4>
                 <h1>Starred boards</h1>
             </section>
-            <BoardPreview count={2}/>
-        </section>
-        <section className="board-container">
+            <BoardPreview count={starred} />
+        </section>}
+        {myBoards > 0 && <section className="board-container">
             <section className="board-preview-header">
                 <h4>icon</h4>
                 <h1>My boards</h1>
             </section>
-            <BoardPreview count={1}/>
-        </section>
-        <section className="board-container">
+            <BoardPreview count={myBoards} />
+        </section>}
+        {templates > 0 && <section className="board-container">
             <section className="board-preview-header">
                 <h4>icon</h4>
                 <h1>Templates</h1>
             </section>
-            <BoardPreview count={5}/>
-        </section>
+            <BoardPreview count={templates} />
+        </section>}
     </div>
 }
