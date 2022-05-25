1164fff4eff0c19cca2e3eacad497368fd38f7ad
Itay Zonshine <ezonsh@email.com>
2022-05-25 12:46:21 +0300

    service work

--- src/store/board/board.action.js
+++ src/store/board/board.action.js
@@ -1,22 +1,21 @@
 // import { showSuccessMsg } from '../services/event-bus.service'
-import { boardService } from '../services/board.service'
-import { userService } from '../services/user.service'
-
-const SCORE_FOR_REVIEW = 500
+import { boardService } from '../../services/board/board.service'
+// import { userService } from '../../services/user.service'
 
 // Action Creators
 export function getActionRemoveBoard(boardId) {
-    return { type: 'REMOVE_REVIEW', boardId }
+    return { type: 'REMOVE_BOARD', boardId }
 }
+
 export function getActionAddBoard(board) {
-    return { type: 'ADD_REVIEW', board }
+    return { type: 'ADD_BOARD', board }
 }
 
 export function loadBoard() {
     return async dispatch => {
         try {
             const board = await boardService.query()
-            dispatch({ type: 'SET_REVIEWS', board })
+            dispatch({ type: 'SET_BOARDS', board })
 
         } catch (err) {
             console.log('BoardActions: err in loadBoard', err)
