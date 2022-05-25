f62bbd621b3a564f11bf3ff4885b1706b4b89eda
Itay Zonshine <ezonsh@email.com>
2022-05-25 21:14:02 +0300

    adding tasks feature

--- src/cmps/board/task-list.jsx
+++ src/cmps/board/task-list.jsx
@@ -1,11 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { TaskPreview } from "./task-preview"
 
 
-export const Group = ({ group }) => {
+export const Group = ({ group, onAddTask }) => {
     const { tasks } = group
-    console.log(group.tasks)
+
+    const [newTask, setNewTask] = useState({ title: '', groupId: group.id })
+    const [isTaskOpen, setIsTaskOpen] = useState(false)
+
+    const handleChange = ({ target }) => {
+        const { value, name } = target
+        setNewTask(prevState => ({ ...prevState, [name]: value }))
+    }
+
+    console.log(newTask)
     return <section className="group flex col">
         <div className="group-header flex space-between">
             <textarea maxLength="521">group's title</textarea>
@@ -15,10 +24,32 @@ export const Group = ({ group }) => {
             {tasks.map(task => {
                 return <TaskPreview task={task} />
             })}
+            {isTaskOpen && <article className="task-preview">
+                <form id="add-card" onSubmit={ev => {
+                    ev.preventDefault()
+                    onAddTask(newTask)
+                    setNewTask({ title: '', groupId: group.id })
+                }}></form>
+                <textarea
+                    type="text"
+                    autoComplete="off"
+                    className="task-title"
+                    name="title"
+                    style={({ width: '100%', height: "54px", overflowX: "hidden", overflowWrap: "break-word", wordBreak: "break-all" })}
+                    onBlur={ev => setIsTaskOpen(false)}
+                    value={newTask.title}
+                    onChange={handleChange}
+                ></textarea>
+            </article>}
         </div>
-        <div className="group-footer flex space-between align-center">
-            <a href="">Add a card</a>
+        {isTaskOpen || <div className="group-footer flex space-between align-center">
+            <button onClick={ev => setIsTaskOpen(true)
+            }>Add a card</button>
             <div className="add-media"></div>
-        </div>
+        </div>}
+        {isTaskOpen && <div className="group-footer flex align-center">
+            <button type="submit" form="add-card">Add card</button>
+            <button onClick={ev => setIsTaskOpen(false)}>X</button>
+        </div>}
     </section>
 }
