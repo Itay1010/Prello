e227de2826c44886da43caaaf363857a090f85f5
Itay Zonshine <ezonsh@email.com>
2022-05-26 12:42:25 +0300

    moving to 3 services in board

--- src/cmps/board/task-preview.jsx
+++ src/cmps/board/task-preview.jsx
@@ -3,16 +3,6 @@ import { Link, useParams } from "react-router-dom"
 
 
 export const TaskPreview = ({ task, groupId, onArchiveTask }) => {
-<<<<<<< HEAD
-
-    return <article className="task-preview">
-        {/* <Link to={'/task/' + task.id}> */}
-        <section className="task-color">task color</section>
-        <section className="task-label"><div className="label"></div></section>
-        <section className="task-title">{task.title}</section>
-        <section className="task-status">status</section>
-        {/* </Link> */}
-=======
     const params = useParams()
     const { boardId } = params
 
@@ -23,7 +13,6 @@ export const TaskPreview = ({ task, groupId, onArchiveTask }) => {
             <section className="task-title">{task.title}</section>
             <section className="task-status">status</section>
         </Link>
->>>>>>> 7a232ba204220dafc8ce950ca88f0e5ad89c29d7
         <button onClick={ev => onArchiveTask({ taskId: task.id, groupId })}>Archive card</button>
     </article>
 }
