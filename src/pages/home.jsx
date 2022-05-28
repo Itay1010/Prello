6746e2b1a5f3344ce2ea9f0769ed4ecd1e779bc8
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-29 01:40:18 +0300

    sync day 5

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,9 +5,12 @@ export const Home = () => {
     return <section className="home">
         <nav className="flex space-between"><div className="logo">Logo</div><div className="auth-links">Auth links</div></nav>
         <div className="hero">
-            <h1>While you grow, save yourself the trouble with Prello</h1>
-            <p>Preparation is the key name, as a freelancer to enterprises Prello is the tool to keep yourself in ease.</p>
-            <button>Come and explore</button>
+            <div className="hero-content">
+                <h1>While you grow, save yourself the trouble with Prello</h1>
+                <p>Preparation is the key name, as a freelancer to enterprises Prello is the tool to keep yourself in ease.</p>
+                <button>Come and explore</button>
+
+            </div>
         </div>
     </section>
 }
