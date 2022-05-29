8afc19a0af2d7bebc626a65d90c617fe93ac9f91
Itay Zonshine <ezonsh@email.com>
2022-05-30 00:06:23 +0300

    sync

--- src/cmps/task-preview/task-labels.jsx
+++ src/cmps/task-preview/task-labels.jsx
@@ -1,5 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 
 export function TaskLabels({ labels }) {
-    return labels.map(label => <div className="label" style={{ backgroundColor: label }} key={label}></div>)
+    const { isExpand, setExpand } = useState(false)
+    return labels.map(label => <div className={`label ${isExpand ? 'expanded' : ''}`} onMouseDown={ev => {
+        ev.stopPropagation()
+        ev.preventDefault()
+        setExpand(prevState => !prevState)
+    }} style={{ backgroundColor: label }} key={label}></div>)
 }
