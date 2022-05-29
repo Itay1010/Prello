4c61f666a810edc8c64a55af954e048d10a8ee4d
Offir Carmi <offircarmi@gmail.com>
2022-05-30 00:09:20 +0300

    sync day 6

--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -24,7 +24,6 @@ class _Board extends React.Component {
 
     componentDidMount() {
         this._setBoard()
-        this.getAvgColor('https://images.unsplash.com/photo-1653759588370-03395fadcbd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1219&q=80')
 
 
 
