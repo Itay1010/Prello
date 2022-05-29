0a166d8556ca7641c1251a1bfc7acc2a9b48a40e
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-05-29 10:35:13 +0300

    sync day 5

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,22 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 import { TaskPreview } from "../cmps/board/task-preview"
+
 
 export const Home = () => {
     return <section className="home">
-        <nav className="flex space-between"><div className="logo">Logo</div><div className="auth-links">Auth links</div></nav>
+        <nav className="flex space-between">
+            <div className="logo">Logo</div>
+            <div className="auth-links"><NavLink>Log in</NavLink><NavLink>Sign up</NavLink></div>
+        </nav>
         <div className="hero">
             <div className="hero-content">
-                <h1>While you grow, save yourself the trouble with Prello</h1>
-                <p>Preparation is the key name, as a freelancer to enterprises Prello is the tool to keep yourself in ease.</p>
+                <h1><span> While you grow, save yourself</span><span> the trouble with Prello</span></h1>
+                <p>Preparation is the key name, as a freelancer to enterprises </p>
+                <p>Prello is the tool to keep yourself in ease.</p>
                 <button>Come and explore</button>
-
             </div>
+            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/spirit/hero/6a3ccd8e5c9a0e8ebea4235d12da6b24/hero.png" alt="" />
         </div>
     </section>
 }
