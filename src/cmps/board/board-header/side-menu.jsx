d505a1d6bd2e9adef34e3c5e44410f97ee1051dd
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-06-01 18:26:27 +0300

    sync

--- src/cmps/board/board-header/side-menu.jsx
+++ src/cmps/board/board-header/side-menu.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 export const SideMenu = () => {
     return <section className='side-menu'>
-        hello from side menu
+        <div className='clear-screen'></div>
+        <div className='menu-content'>
+            <h1>Menu</h1>
+            <div className='buttons-wrapper'>
+                <button>About this board</button>
+                <button>Choose background</button>
+                <button>More</button>
+            </div>
+            <div className='activity-wrapper'></div>
+        </div>
     </section>
 }
