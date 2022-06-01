571ae81bc170ebdc36399b7ce8b392a3928b8caf
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-06-02 01:45:34 +0300

    sync

---
+++ src/cmps/board/board-header/choose-background-modal.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const ChooseBackgroundModal = ({ closeBackgroundModal }) => {
+    return <div className='background-modal'>
+        <h1>hello</h1>
+    </div>
+}
