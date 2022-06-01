4db9c1132b61a1d2422122da3a23f6c0d3e01f81
Offir Carmi <offircarmi@gmail.com>
2022-06-01 21:54:03 +0300

    sync

--- src/cmps/workspace/board-preview.jsx
+++ src/cmps/workspace/board-preview.jsx
@@ -1,21 +1,21 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
+import { IStar } from "../icons/i-star"
 
 export const BoardPreview = ({ board, toggleStar }) => {
     const history = useHistory()
     const onGoToBoard = (boardId) => {
         history.push(`/board/${boardId}`)
     }
-
+    console.log('board', board);
     const background = board.style.background ? board.style.background : board.style.backgroundColor
-    return (<div className="board-preview-body" onClick={() => onGoToBoard(board._id)} style={{ backgroundImage: `url('${background})` }}>
+    return (<div className={board.isStarred ? "board-preview-body starred" : "board-preview-body"} onClick={() => onGoToBoard(board._id)} style={{ backgroundImage: `url('${background})` }}>
         <h1>{board.title}</h1>
         <div className="star-wrapper" onClick={(event) => {
             event.stopPropagation()
             toggleStar(board)
-        }
-        }
-        >star</div>
+        }}> <IStar /> </div>
+
     </div>
     )
 }
