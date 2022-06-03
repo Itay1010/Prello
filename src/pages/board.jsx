3b6e6e6925e17b1cc35db222894544ea4c382f02
Itay Zonshine <ezonsh@email.com>
2022-06-03 22:10:23 +0300

    sync

--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -97,7 +97,15 @@ class _Board extends React.Component {
         const newBoard = JSON.parse(JSON.stringify(this.props.board))
         const groupIdx = newBoard.groups.findIndex(group => group.id === newTask.groupId)
 
-        newTask = { id: utilService.makeId(), title: newTask.title, style: { backgroundImg: '', bgColor: '', size: '' } }
+        newTask = {
+            id: utilService.makeId(),
+            title: newTask.title,
+            style: { backgroundImg: '', bgColor: '', size: '', },
+            members: [],
+            attachments: [],
+            labels: [],
+            checklist: []
+        }
         newBoard.groups[groupIdx].tasks.push(newTask)
         actService.activity('added', 'card,', newTask, newBoard)
         await this.props.updateBoard(newBoard)
