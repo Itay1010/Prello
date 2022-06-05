04642aaa24e39dd2002ad44d65669305da8cac61
Itay Zonshine <ezonsh@email.com>
2022-06-05 11:46:28 +0300

    sync

--- src/cmps/board/board-header/members-cmps/board-members.jsx
+++ src/cmps/board/board-header/members-cmps/board-members.jsx
@@ -8,7 +8,7 @@ export const BoardMembers = ({ board, onChangeMembers }) => {
 
     const [selectedUser, setUser] = useState(null)
     const [isSocialOpen, setSocial] = useState(false)
-    const [isMoreOpen, setMoreOpem] = useState(true)
+    const [isMoreOpen, setMoreOpen] = useState(false)
     const [users, setUsers] = useState(null)
     const limit = 4
 
@@ -44,9 +44,14 @@ export const BoardMembers = ({ board, onChangeMembers }) => {
                 setUser={setUser}
                 onRemoveMember={onChangeMembers}
             />}
-            {/* <MoreMembersModal members={memberToDisplay} /> */}
+            {isMoreOpen && leftOvers.length > 0 && <MoreMembersModal members={leftOvers} />}
         </section>
-        {leftOvers.length && <button className='extra-btn reset'>{`+${leftOvers.length}`}</button>}
+        {
+            leftOvers.length > 0 && <button className='extra-btn reset' onClick={ev => setMoreOpen(prevState => !prevState)}>
+                {`+${leftOvers.length}`}
+            </button>
+        }
+
         {isSocialOpen && <div className='members-screen' onClick={ev => setSocial(false)}></div>}
         <button className='share-board reset' onClick={ev => setSocial(prevState => !prevState)}>
             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M376 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z"></path><path fill="none" strokeMiterlimit="10" strokeWidth="32" d="M288 304c-87 0-175.3 48-191.64 138.6-2 10.92 4.21 21.4 15.65 21.4H464c11.44 0 17.62-10.48 15.65-21.4C463.3 352 375 304 288 304z"></path><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M88 176v112m56-56H32"></path>
